566f756b3af023ed129e09f74ee389bb33dcc37c
NemethAdam01 <nemeth.adam@diak.szbi-pg.hu>
2025-12-12 08:44:31 +0100

    Update alap_ellenorzes.js

--- alap_ellenorzes.js
+++ alap_ellenorzes.js
@@ -1,1 +1,24 @@
 // /^[a-zA-ZáéíóöőúüűÁÉÍÓÖŐÚÜŰ ]+$/
+
+function ellenorAlap()
+{
+    let nev = document.forms["alapUrlap"]["nev"].value
+    console.log(nev)
+
+    if(nev.trim() === "")
+        {
+        alert("A név mező nem lehet üres!")
+        return false
+        }
+        
+    let nevFilter = /^[a-zA-ZáéíóöőúüűÁÉÍÓÖŐÚÜŰ ]+$/
+    if(!nevFilter.test(nev))
+        {
+        alert("A név mező csak betűket és szóközt tartalmazhat!")
+        return false
+        }
+
+    let nevKiirasa = document.getElementById("nevEredmeny")
+    nevKiirasa.innerText = "A megadott név: " + nev
+    return false
+}
